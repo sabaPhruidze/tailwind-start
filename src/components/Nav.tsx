b12c63399d48a1d08bef29006cc861daca07bf02
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-18 19:36:57 +0400

    I created the nav bar and I also made it possible when to show the icon and when to hide it

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import HeaderLogo from "../assets/images/headerLogo.svg";
+import hamburger from "../assets/icons/hamburger.svg";
+import { navLinks, Navlinks } from "../constants/index";
 
-const fegerg = () => {
-  return <div>fegerg</div>;
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={HeaderLogo} alt="header logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item: Navlinks) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <img
+            src={hamburger}
+            alt="hamburger"
+            width={25}
+            height={25}
+            className="hidden max-lg:block"
+          />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
-export default fegerg;
+export default Nav;

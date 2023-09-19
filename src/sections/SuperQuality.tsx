c3d84f2316add7de255df179b514ee506bafab8e
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-19 17:10:20 +0400

    starting the superQuality part

--- src/sections/SuperQuality.tsx
+++ src/sections/SuperQuality.tsx
@@ -1,5 +1,10 @@
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    ></section>
+  );
 };
 
 export default SuperQuality;

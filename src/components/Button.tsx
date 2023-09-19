6b7a3c6334d3a6211be0e6aa11b90f22d8592eab
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-19 17:26:45 +0400

    I finished the left part , there was something I had to correct in the button component , so there is time when I give iconURL props to it and for the supequality component I do not want it to display any image So I changed the Button component little bit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,20 @@ import React from "react";
 
 interface Props {
   label: string;
-  iconURL: string;
+  iconURL?: string;
 }
 const Button = (props: Props) => {
   const { label, iconURL } = props;
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img src={iconURL} alt="icon url" className="ml-2 rounded-full w-5 h-5" />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="icon url"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };

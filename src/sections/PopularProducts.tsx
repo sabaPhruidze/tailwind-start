f756c439b9ca0785f7a5bc4fb4459635c5e911a8
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-19 16:21:12 +0400

    I finished popular products as well

--- src/sections/PopularProducts.tsx
+++ src/sections/PopularProducts.tsx
@@ -1,5 +1,5 @@
 import { products, Products } from "../constants";
-
+import PopularProductCard from "../components/PopularProductCard";
 const PopularProducts = () => {
   return (
     <section id="products" className="max-container max-sm:mt-12">
@@ -13,6 +13,11 @@ const PopularProducts = () => {
           Experience top-notch quality and style with out sought-after
           selections.Discover a world of comfort, design, and value
         </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product: Products) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
       </div>
     </section>
   );

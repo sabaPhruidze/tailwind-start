29d0bc0c4350f84555a6f75aaecf27f0ba91fbaf
Saba Phruidze <sabaphruidze01@gmail.com>
2023-09-21 12:27:18 +0400

    I am doing subscribe part and then I will do the footer but I think I will not finish only by this. I will add a simple motions from framer motion , in order to make it more stylish

--- src/sections/Subscribe.tsx
+++ src/sections/Subscribe.tsx
@@ -1,5 +1,15 @@
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+      id="contact-us"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-bold font-palanquin">
+        Sign Up from
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+    </section>
+  );
 };
 
 export default Subscribe;
